f67c26f1a64c98f3cdcd2269cddc20862bc29feb
sunerok <justinvforvendetta@gmail.com>
2024-08-25 19:22:23 -0400

    drop to 2.1.8

--- libevent.rb
+++ libevent.rb
@@ -1,16 +1,18 @@
 class Libevent < Formula
   desc "Asynchronous event library"
-  homepage "https://libevent.org/"
-  url "https://github.com/libevent/libevent/archive/release-2.1.11-stable.tar.gz"
-  sha256 "229393ab2bf0dc94694f21836846b424f3532585bac3468738b7bf752c03901e"
-  revision 1
+  homepage "http://libevent.org"
+  url "https://github.com/libevent/libevent/archive/release-2.1.8-stable.tar.gz"
+  sha256 "316ddb401745ac5d222d7c529ef1eada12f58f6376a66c1118eee803cb70f83d"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "doxygen" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
-  depends_on "openssl@1.1"
+  depends_on "openssl"
+
+  conflicts_with "pincaster",
+    :because => "both install `event_rpcgen.py` binaries"
 
   def install
     inreplace "Doxyfile", /GENERATE_MAN\s*=\s*NO/, "GENERATE_MAN = YES"
@@ -22,11 +24,10 @@ class Libevent < Formula
     system "make", "install"
     system "make", "doxygen"
     man3.install Dir["doxygen/man/man3/*.3"]
-    doc.install Dir["doxygen/html/*"]
   end
 
   test do
-    (testpath/"test.c").write <<~EOS
+    (testpath/"test.c").write <<-EOS.undent
       #include <event2/event.h>
 
       int main()
